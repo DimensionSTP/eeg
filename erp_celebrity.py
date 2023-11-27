78f7c00252241ebc089f974e26105b53d8abef38
DimensionSTP <ddang8jh@gmail.com>
2023-11-27 22:42:52 +0900

    add celebrity task

---
+++ erp_celebrity.py
@@ -0,0 +1,210 @@
+import os
+from typing import List
+from datetime import datetime
+
+import pandas as pd
+
+from src.task import celebrity_task
+from src.analysis import AnalyzeEEG
+from src.plot import PlotEEG
+from src.recommendation import recommend_celebrity
+
+
+def erp_celebrity(
+    screen_width: int,
+    screen_height: int,
+    fs: int,
+    channels: List,
+    isi: int,
+    background_path: str,
+    sex: str,
+    image_folder: str,
+    num_trials: int,
+    num_images: int,
+    event_save_path: str,
+    result_dir: str,
+):
+    today = str(datetime.now().date())
+    if not os.path.exists(f"./data/{today}"):
+        os.makedirs(f"./data/{today}")
+    if not os.path.exists(f"./event/{today}"):
+        os.makedirs(f"./event/{today}")
+
+    celebrity_task(
+        screen_width=screen_width,
+        screen_height=screen_height,
+        isi=isi,
+        background_path=background_path,
+        image_folder=image_folder,
+        num_trials=num_trials,
+        num_images=num_images,
+        event_save_path=f"{event_save_path}/{today}",
+        sex=sex
+    )
+
+    rawdata_folders = os.listdir("C:/MAVE_RawData")
+
+    text_file_name = f"C:/MAVE_RawData/{rawdata_folders[-1]}/Rawdata.txt"
+    data_df = pd.read_csv(text_file_name, delimiter="\t")
+
+    record_start_time = data_df.iloc[0, 0]
+    hour = str(record_start_time).split(":")[0]
+    min = str(record_start_time).split(":")[1]
+    sec = str(record_start_time).split(":")[2].split(".")[0]
+
+    data_df = data_df[channels]
+    data_file_path = f"./data/{today}/Rawdata_{hour}.{min}.{sec}.csv"
+    data_df.to_csv(data_file_path, index=False)
+
+    event_paths = os.listdir(f"./event/{today}")
+    event_file = f"./event/{today}/{event_paths[-1]}"
+
+    analyze_eeg = AnalyzeEEG(channels=channels, fs=fs)
+    eeg, eeg_times, avg_evoked_list, times_list = analyze_eeg.analyze_erp(
+        eeg_filename=data_file_path,
+        event_filename=event_file,
+        result_dir=result_dir,
+        num_types=num_images,
+    )
+
+    plot_eeg = PlotEEG(
+        channels=channels,
+        result_dir=result_dir,
+        is_show=False,
+        is_save=True,
+        eeg=eeg,
+        eeg_times=eeg_times,
+        eeg_filename="eeg_raw",
+    )
+    plot_eeg.plot_eeg()
+    if sex == "males":
+        for i in range(num_images):
+            plot_eeg.plot_electrode(
+                avg_evoked_list[i],
+                times_list[i],
+                filename=f"males_{i+1}_electrode",
+            )
+    elif sex == "females":
+        for i in range(num_images):
+            plot_eeg.plot_electrode(
+                avg_evoked_list[i],
+                times_list[i],
+                filename=f"females_{i+1}_electrode",
+            )
+    else:
+        raise ValueError("Invalid clothes type")
+
+    recommend_celebrity(
+        avg_evoked_list=avg_evoked_list,
+        times_list=times_list,
+        channels=channels,
+        sex=sex,
+        image_folder=image_folder,
+        result_dir=result_dir,
+        screen_width=screen_width,
+        screen_height=screen_height
+    )
+
+
+if __name__ == "__main__":
+    import argparse
+    import ast
+
+    def parse_list(string: str):
+        try:
+            parsed_list = ast.literal_eval(string)
+            if isinstance(parsed_list, list):
+                return parsed_list
+            else:
+                raise argparse.ArgumentTypeError("Invalid list format")
+        except (ValueError, SyntaxError):
+            raise argparse.ArgumentTypeError("Invalid list format")
+
+    parser = argparse.ArgumentParser(
+        description="Insert arguments for function of erp combination"
+    )
+    parser.add_argument(
+        "--screen_width",
+        type=int,
+        default=1920,
+        help="Set screen width of combination task",
+    )
+    parser.add_argument(
+        "--screen_height",
+        type=int,
+        default=1080,
+        help="Set screen height of combination task",
+    )
+    parser.add_argument(
+        "--fs", type=int, default=256, help="Get resolution of EEG device"
+    )
+    parser.add_argument(
+        "--channels",
+        type=parse_list,
+        default="['EEG_Fp1', 'EEG_Fp2']",
+        help="Get channels of EEG device",
+    )
+    parser.add_argument(
+        "--isi",
+        type=int,
+        default=1000,
+        help="Set inter-stimulus interval of combination task",
+    )
+    parser.add_argument(
+        "--image_path",
+        type=str,
+        default="./images",
+        help="Get image data path to use in the task",
+    )
+    parser.add_argument(
+        "--sex",
+        type=str,
+        default="males",
+        help="Set type of clothes to use in the task",
+    )
+    parser.add_argument(
+        "--num_trials",
+        type=int,
+        default=10,
+        help="Set number of trials to use in the task",
+    )
+    parser.add_argument(
+        "--num_images",
+        type=int,
+        default=11,
+        help="Set number of clothes to use in the task",
+    )
+    parser.add_argument(
+        "--event_save_path",
+        type=str,
+        default="./event",
+        help="Set a record of events file saving path",
+    )
+    parser.add_argument(
+        "--result_dir",
+        type=str,
+        default="./plot",
+        help="Set a EEG, ERP plots saving path",
+    )
+    parser.add_argument(
+        "--result_dir_num",
+        type=int,
+        default=0,
+        help="Set a EEG, ERP plots detailed saving path",
+    )
+    args = parser.parse_args()
+
+    erp_celebrity(
+        screen_width=args.screen_width,
+        screen_height=args.screen_height,
+        isi=args.isi,
+        fs=args.fs,
+        channels=args.channels,
+        background_path=f"{args.image_path}/backgrounds/B0.jpg",
+        image_folder=f"{args.image_path}/{args.sex}",
+        sex=f"{args.sex}",
+        num_trials=args.num_trials,
+        num_images=args.num_images,
+        event_save_path=f"{args.event_save_path}",
+        result_dir=f"{args.result_dir}/{args.sex}/{args.result_dir_num}",
+    )
--- src/recommendation.py
+++ src/recommendation.py
@@ -68,6 +68,160 @@ def recommend_combination(
         raise ValueError("Invalid clothes type")
 
 
+def recommend_celebrity(
+    avg_evoked_list: List, times_list: List, channels: List, sex: str, image_folder: str, result_dir: str, screen_width: int, screen_height: int,
+):
+    male_celebrities = [
+        "공유",
+        "송중기",
+        "박서준",
+        "유연석",
+        "이종석",
+        "김선호",
+        "정해인",
+        "이제훈",
+        "이동욱",
+        "뷔",
+        "차은우"
+    ]
+    female_celebrities = [
+        "김유정",
+        "전소미",
+        "한소희",
+        "수지",
+        "안유진",
+        "카리나",
+        "윈터",
+        "미연",
+        "태연",
+        "아이유",
+        "윤아"
+    ]
+    max_values_per_channels = []
+    for channel_idx in range(len(channels)):
+        max_values = []
+        for time in range(len(times_list)):
+            selected_indices = [
+                index
+                for index, value in enumerate(times_list[time])
+                if 0.1 <= value <= 0.5
+            ]
+            start_index = selected_indices[0]
+            end_index = selected_indices[-1]
+
+            max_value = max(
+                avg_evoked_list[time][channel_idx][start_index : end_index + 1]
+            )
+            max_values.append(max_value)
+        max_values_per_channels.append(max_values)
+
+    indices_of_largest_values_per_channels = []
+    for channel in range(len(max_values_per_channels)):
+        indices_of_largest_values = sorted(
+            range(len(max_values_per_channels[channel])),
+            key=lambda i: max_values_per_channels[channel][i],
+            reverse=True,
+        )[:3]
+        largest_values = [
+            max_values_per_channels[channel][i] for i in indices_of_largest_values
+        ]
+        top_values_and_indices = [
+            (value, index)
+            for value, index in zip(largest_values, indices_of_largest_values)
+        ]
+        indices_of_largest_values_per_channels.append(top_values_and_indices)
+
+    top_values_and_indices = sum(indices_of_largest_values_per_channels, [])
+    sorted_top_values_and_indices = sorted(
+        top_values_and_indices, key=lambda i: i[0], reverse=True
+    )
+    top_index = sorted_top_values_and_indices[0][1]
+    erp_fp1_path = f"{result_dir}/{sex}_{top_index+1}_electrode_average_EEG_Fp1.png"
+    erp_fp2_path = f"{result_dir}/{sex}_{top_index+1}_electrode_average_EEG_Fp2.png"
+    # Let"s start fresh and simply vertically stack the 2nd and 3rd images as requested by the user.
+
+    # Reload the EEG images
+    erp_fp1_plot = Image.open(erp_fp1_path)
+    erp_fp2_plot = Image.open(erp_fp2_path)
+
+    # Create a new image with the combined height of the two EEG images and the width of the widest one
+    vertical_combined_height = erp_fp1_plot.height + erp_fp2_plot.height
+    vertical_combined_width = max(erp_fp1_plot.width, erp_fp2_plot.width)
+
+    # Create a new image with a black background
+    vertical_combined_image = Image.new("RGB", (vertical_combined_width, vertical_combined_height), "black")
+
+    # Paste the first EEG image at the top
+    vertical_combined_image.paste(erp_fp1_plot, (0, 0))
+
+    # Paste the second EEG image directly below the first
+    vertical_combined_image.paste(erp_fp2_plot, (0, erp_fp1_plot.height))
+
+    # Save the vertically combined image
+    vertical_combined_image_path = f"{result_dir}/erp_combined.png"
+    vertical_combined_image.save(vertical_combined_image_path)
+
+    # Load images
+    if sex == "males":
+        celebrity_path = f"{image_folder}/M{top_index+1}.jpg"
+        text = f"당신이 끌리는 연예인은 {male_celebrities[top_index]}입니다."
+    elif sex == "females":
+        celebrity_path = f"{image_folder}/F{top_index+1}.jpg"
+        text = f"당신이 끌리는 연예인은 {female_celebrities[top_index]}입니다."
+    else:
+        raise ValueError("Invalid sex")
+    erp_combined_path = f"{result_dir}/erp_combined.png"
+    celebrity_image = Image.open(celebrity_path)
+    erp_combined_plot = Image.open(erp_combined_path)
+
+    # Determine the new width and height for the combined image
+    new_width = max(celebrity_image.width + erp_combined_plot.width, screen_width)
+    new_height = max(celebrity_image.height, erp_combined_plot.height, screen_height)
+
+    # Create a new image with the determined dimensions and black background
+    combined_image = Image.new("RGB", (new_width, new_height), color="black")
+
+    # Calculate the position for the first image (left)
+    x_offset = int((new_width - (celebrity_image.width + erp_combined_plot.width)) / 2)
+    y_offset = int((new_height - celebrity_image.height) / 2)
+
+    # Paste the first image onto the combined image
+    combined_image.paste(celebrity_image, (x_offset, y_offset))
+
+    # Calculate the position for the second image (right)
+    x_offset += celebrity_image.width
+
+    # Paste the second image onto the combined image
+    combined_image.paste(erp_combined_plot, (x_offset, y_offset))
+
+    # Add text to the combined image
+    draw = ImageDraw.Draw(combined_image)
+    font_size = 50  # Starting font size
+    font_path = "C:/Windows/Fonts/batang.ttc"
+    font = ImageFont.truetype(font_path, font_size)
+
+    # Calculate the width and height of the text to be added
+    text_width, text_height = draw.textsize(text, font=font)
+
+    # While the text width is too large, reduce the font size
+    while text_width > new_width and font_size > 10:
+        font_size -= 1
+        font = ImageFont.truetype(font_path, font_size)
+        text_width, text_height = draw.textsize(text, font=font)
+
+    # Calculate the position for the text
+    text_x = (new_width - text_width) / 2
+    text_y = 10  # A small padding from the top
+
+    # Draw the text onto the combined image
+    draw.text((text_x, text_y), text, font=font, fill="white")
+
+    # Save the combined image
+    combined_image_path = f"{result_dir}/recommendation.png"
+    combined_image.save(combined_image_path)
+    combined_image.show()
+
+
 def recommend_answer_ssvep(
     fp1_df:pd.DataFrame, fp2_df:pd.DataFrame, screen_width: int, screen_height: int, frequencies: List, image_folder: str, correct_num: int, result_dir: str
 ):
