b7c1ca71c215347e7885689009d32bb6b00300f6
DimensionSTP <ddang8jh@gmail.com>
2024-03-09 01:40:01 +0900

    update: alarm by English

--- resize_images.py
+++ resize_images.py
@@ -10,11 +10,10 @@ def resize_images(source_folder: str, destination_folder: str):
         if filename.endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif")):
             img_path = os.path.join(source_folder, filename)
             img = Image.open(img_path)
-            # 이미지 리사이즈
             img_resized = img.resize((280, 280), Image.ANTIALIAS)
             img_resized.save(os.path.join(destination_folder, filename))
 
-    print("모든 이미지가 리사이즈되어 bws 폴더에 저장되었습니다.")
+    print("All images have been resized and saved in the bws folder.")
 
 
 if __name__ == "__main__":
